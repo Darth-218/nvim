65ca24c7d612344799d29586dace8682e74edc1e
Darth218 <64696569+Darth-218@users.noreply.github.com>
2024-03-09 19:41:05 +0200

    Added different keybinds for different modes

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -4,12 +4,16 @@ return {
     version = "*",
     opts = {
       shell = "zsh",
-      open_mapping = [[<c-\>]],
+      open_mapping = [[<c-->]],
       hide_numbers = true,
       autochdir = true,
       border = "double",
       auto_scroll = true,
-      direction = "float",
+      direction = "horizontal",
     },
   },
+
+  vim.keymap.set("n", "<C-\\>f", "<cmd>2ToggleTerm direction=float<CR>", { noremap = true, silent = true }),
+  vim.keymap.set("n", "<C-\\>v", "<cmd>3ToggleTerm direction=vertical<CR>", { noremap = true, silent = true }),
+  vim.keymap.set("n", "<C-\\>h", "<cmd>3ToggleTerm direction=horizontal<CR>", { noremap = true, silent = true }),
 }
