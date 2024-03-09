775c0bfa8263d0b118d57b77c7fc922f85c09b58
Darth218 <64696569+Darth-218@users.noreply.github.com>
2024-03-09 12:02:31 +0200

    Added toggleterm plugin for more terminal functionality

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+      shell = "zsh",
+      open_mapping = [[<c-\>]],
+      hide_numbers = true,
+      autochdir = true,
+      border = "double",
+      auto_scroll = true,
+      direction = "float",
+    },
+  },
+}
