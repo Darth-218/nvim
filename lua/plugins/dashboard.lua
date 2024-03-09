d9d68a7fc03d694b779d0b28abd399370065a043
Darth218 <64696569+Darth-218@users.noreply.github.com>
2024-03-09 18:48:47 +0200

    Created a dashboard

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,49 @@
+local logo = [[
+
+
+  ██████╗  █████╗ ███╗   ██╗███████╗███████╗    ██╗   ██╗██╗███╗   ███╗
+  ██╔══██╗██╔══██╗████╗  ██║██╔════╝██╔════╝    ██║   ██║██║████╗ ████║
+  ██████╔╝███████║██╔██╗ ██║█████╗  ███████╗    ██║   ██║██║██╔████╔██║
+  ██╔══██╗██╔══██║██║╚██╗██║██╔══╝  ╚════██║    ╚██╗ ██╔╝██║██║╚██╔╝██║
+  ██████╔╝██║  ██║██║ ╚████║███████╗███████║     ╚████╔╝ ██║██║ ╚═╝ ██║
+  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝╚══════╝╚══════╝      ╚═══╝  ╚═╝╚═╝     ╚═╝
+                                                                     
+
+          ]]
+
+logo = string.rep("\n", 8) .. logo .. "\n\n"
+
+return {
+
+  "nvimdev/dashboard-nvim",
+
+  event = "VimEnter",
+
+  config = function()
+    require("dashboard").setup({
+
+      theme = "hyper",
+      shortcut_type = "letter",
+
+      preview = { file_path = true },
+
+      config = {
+
+        header = vim.split(logo, "\n"),
+
+        mru = { limit = 5 },
+
+        shortcut = {
+
+          { desc = "󰤘 Recent-Files", action = "Telescope oldfiles", key = "r" },
+          { desc = "󰈔 Files", action = "Telescope find_files", key = "f" },
+          { desc = " Git-Files", action = "Telescope git_files", key = "g" },
+          { desc = "󰉦 Theme", action = "Telescope colorscheme", key = "t" },
+          { desc = " Find-In-File", action = "Telescope live_grep", key = "l" },
+        },
+        footer = { " " },
+      },
+    })
+  end,
+  dependencies = { { "nvim-tree/nvim-web-devicons" } },
+}
