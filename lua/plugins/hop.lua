ec5994b5b8f0eb766cf6b0569b8b7ccd0b8c09fa
Darth218 <64696569+Darth-218@users.noreply.github.com>
2024-03-15 15:40:13 +0200

    Added the hop plugin for better navigation

---
+++ lua/plugins/hop.lua
@@ -0,0 +1,24 @@
+return {
+
+  "phaazon/hop.nvim",
+
+  opts = {
+
+    multi_windows = true,
+    keys = "htnsueoaidgcrlypmbkjvx",
+    uppercase_labels = true,
+  },
+
+  keys = {
+
+    {
+      "<leader>fj",
+
+      function()
+        require("hop").hint_words()
+      end,
+
+      mode = { "n", "x", "o" },
+    },
+  },
+}
