d09c68f88af18d884cf6fd958489eb9a7cb5d345
Darth218 <64696569+Darth-218@users.noreply.github.com>
2024-04-16 02:07:29 +0200

    Added neorg

---
+++ lua/plugins/neorg.lua
@@ -0,0 +1,17 @@
+return {
+
+	{
+
+		"vhyrro/luarocks.nvim",
+		priority = 1000,
+		config = true,
+	},
+	{
+
+		"nvim-neorg/neorg",
+		dependencies = {"luarocks.nvim"},
+		lazy = false,
+		version = "*",
+		config = true,
+	},
+}
