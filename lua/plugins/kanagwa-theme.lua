276cc30c0a43162a8f28022d0709b94d37555212
Darth218 <64696569+Darth-218@users.noreply.github.com>
2024-04-16 02:06:22 +0200

    Added line number colors

--- lua/plugins/kanagwa-theme.lua
+++ lua/plugins/kanagwa-theme.lua
@@ -1,9 +1,13 @@
 return {
 	"rebelot/kanagawa.nvim",
 
-	config = function(plugin)                                                                
-		vim.opt.rtp:append(plugin.dir .. "/packages/neovim")                                   
-		vim.cmd([[colorscheme kanagawa-dragon]])                                               
+	config = function(plugin)
+		vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
+		vim.cmd([[colorscheme kanagawa-dragon]])
+		vim.api.nvim_set_hl(0, "LineNrAbove", {fg="#d9a594", bold = false})
+		vim.api.nvim_set_hl(0, "LineNr", {fg="#FF9E3B", bold = true})
+		vim.api.nvim_set_hl(0, "LineNrBelow", {fg="#d9a594", bold = false})
+		vim.wo.relativenumber = true
 	end,
 
 }
