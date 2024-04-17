d1395329001e9857e668f7cf5b40f3d901d67c6b
Darth218 <64696569+Darth-218@users.noreply.github.com>
2024-04-17 18:39:22 +0200

    Modified oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,20 +2,24 @@ return {
   'stevearc/oil.nvim',
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {
-		default_file = true,
-		columns = {"icon", "size", "permissions", },
-		keymaps = {
-			["H"] = "actions.parent",
-			["L"] = "actions.select",
-			["q"] = "actions.close",
-		},
-		view_options = {
-			show_hidden = true,
-		},
-		float = {
-			padding = 5,
-			max_height = 15,
-			max_width = 50,
-		},
-	},
+    default_file = true,
+    columns = {"icon", "size", "permissions", },
+    keymaps = {
+      ["H"] = "actions.parent",
+      ["L"] = "actions.select",
+      ["q"] = "actions.close",
+      ["<Esc>"] = "actions.close",
+    },
+    view_options = {
+      show_hidden = true,
+    },
+    float = {
+      padding = 5,
+      max_height = 25,
+      max_width = 70,
+      win_options = {
+	winblend = 5,
+      },
+    },
+  },
 }
