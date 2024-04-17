f9548bd32368c4a294815bc0e2fa5603653f310e
Darth218 <64696569+Darth-218@users.noreply.github.com>
2024-04-17 18:39:57 +0200

    Modified telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,21 +3,30 @@ return {
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.6",
 
-		dependancies = {"nvim-lua/plenary.nvim"},
+		dependancies = { "nvim-lua/plenary.nvim" },
 	},
 	{
 		"nvim-telescope/telescope-ui-select.nvim",
 
 		config = function()
+			require("telescope").setup({
+				pickers = {
+					find_files = {
+						"fd",
+						".",
+						"--type",
+						"file",
+						"--hidden",
+						"--strip-cwd-prefix",
+					},
+				},
+			})
 			extensions = {
 				["ui-select"] = {
-					require("telescope.themes").get_dropdown {
-						-- even more opts
-					}
-
-				}
+					require("telescope.themes").get_ivy({}),
+				},
 			}
 			-- require("telescope").load_extension("ui-select")
-		end
+		end,
 	},
 }
