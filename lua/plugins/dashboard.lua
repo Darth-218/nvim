cfbcff903dd19513ff1a9a11117caeec49803449
Darth218 <64696569+Darth-218@users.noreply.github.com>
2024-04-17 18:37:57 +0200

    Modified the dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -7,7 +7,7 @@ local logo = [[
   ╚██╗ ██╔╝██║██║╚██╔╝██║
    ╚████╔╝ ██║██║ ╚═╝ ██║
     ╚═══╝  ╚═╝╚═╝     ╚═╝
-                                                                     
+
 
           ]]
 
@@ -15,36 +15,41 @@ logo = string.rep("\n", 8) .. logo .. "\n\n"
 
 return {
 
-  "nvimdev/dashboard-nvim",
+	"nvimdev/dashboard-nvim",
 
-  event = "VimEnter",
+	event = "VimEnter",
 
-  config = function()
-    require("dashboard").setup({
+	config = function()
+		require("dashboard").setup({
 
-      theme = "hyper",
-      shortcut_type = "letter",
+			theme = "hyper",
+			shortcut_type = "letter",
 
-      preview = { file_path = true },
+			preview = { file_path = true },
 
-      config = {
+			config = {
 
-        header = vim.split(logo, "\n"),
+				header = vim.split(logo, "\n"),
 
-	project = {enabled = true, limit = 2},
-        mru = { limit = 5 },
+				project = { enabled = true, limit = 2 },
+				mru = { limit = 5 },
 
-        shortcut = {
+				shortcut = {
 
-          { desc = "󰤘 Recent-Files", action = "Telescope oldfiles", key = "r" },
-          { desc = "󰈔 Files", action = "Files", key = "f" },
-          { desc = " Git-Files", action = "Telescope git_files", key = "g" },
-          { desc = " Find-In-File", action = "Telescope live_grep", key = "l" },
-          { desc = "󰈆 Quit", action = ":q", key = "q" },
-        },
-        footer = { " " },
-      },
-    })
-  end,
-  dependencies = { { "nvim-tree/nvim-web-devicons" } },
+					{ desc = "󰤘 Recent-Files", action = "Telescope oldfiles theme=ivy", key = "r" },
+					{ desc = "󰈔 Files", action = "Telescope find_files theme=ivy hidden=true", key = "f" },
+					{ desc = " Git-Files", action = "Telescope git_files theme=ivy", key = "g" },
+					{ desc = " Find-In-File", action = "Telescope live_grep theme=ivy", key = "l" },
+					{
+						desc = "Restore Session",
+						action = [[<cmd>lua require("persistence").load({ last = true })<cr>]],
+						key = "s",
+					},
+					{ desc = "󰈆 Quit", action = ":q", key = "q" },
+				},
+				footer = { " " },
+			},
+		})
+	end,
+	dependencies = { { "nvim-tree/nvim-web-devicons" } },
 }
