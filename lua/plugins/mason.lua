769db98730f7e535837a6b94dc9f562451f26cc0
Darth218 <64696569+Darth-218@users.noreply.github.com>
2024-04-17 18:38:42 +0200

    Modified mason

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,38 +1,42 @@
 return {
 
-	{
-		"williamboman/mason.nvim",
-		config = function()
+  {
+    "williamboman/mason.nvim",
 
-			require("mason").setup()
-		end
+    lazy = false,
 
-	},
+    config = function()
+      require("mason").setup()
+    end,
+  },
 
-	{
-		"williamboman/mason-lspconfig.nvim",
-
-		config = function()
-
-			require("mason-lspconfig").setup({
-
-				ensure_installed = {
-
-					"lua_ls",
-					"clangd",
-					"ruff",
-				},
-			})
-		end
-	},
-	{
-		"neovim/nvim-lspconfig",
-
-		config = function()
-
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-		end
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+	ensure_installed = {
+	  "lua_ls",
+	  "clangd"
 	}
+      })
+    end
+  },
 
-}
+  {
+    "neovim/nvim-lspconfig",
+
+    config = function()
+
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.clangd.setup({
+	on_attach = function(client, bufnr)
+	  client.server_capabilities.signatureHelpProvider = false
+	  on_attach(client, bufnr)
+	end,
+	capabilities = capabilities
+      })
+    end
+  },
+
+  }
