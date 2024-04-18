e03d4cdeb8c3ad393d664a43b42008688c70efb3
Darth218 <64696569+Darth-218@users.noreply.github.com>
2024-04-19 01:04:58 +0200

    Modified bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,6 +1,39 @@
 return {
-	'akinsho/bufferline.nvim',
-	
-	version = "*",
-	dependencies = 'nvim-tree/nvim-web-devicons'
+  'akinsho/bufferline.nvim',
+
+  version = "*",
+  dependencies = 'nvim-tree/nvim-web-devicons',
+
+  config = function()
+
+    require("bufferline").setup({
+
+      options = {
+	color_icons = false,
+	show_buffer_close_icons = false,
+	diagnostics = "nvim_lsp",
+	show_duplicate_prefix = true,
+	modified_icon = '<~',
+
+	indicator = {
+	  icon = "",
+	  style = 'none'
+	},
+
+	offsets = {
+
+	  {
+	    text_align = "center",
+	    separator = true,
+	  }
+	},
+
+	hover = {
+	  enabled = true,
+	  delay = 100,
+	  reveal = {'close'}
+	}
+      }
+    })
+  end
 }
