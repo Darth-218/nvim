a18a9021f49ba7c9bd63e6ac667786e478119a29
Darth218 <64696569+Darth-218@users.noreply.github.com>
2024-04-23 19:59:57 +0200

    Added zenmode

---
+++ lua/plugins/zenmode.lua
@@ -0,0 +1,24 @@
+return {
+
+  "folke/zen-mode.nvim",
+
+  opts = {
+
+    window = {
+
+      width = .5,
+
+      options = {
+
+	number = true,
+      },
+    },
+
+    plugins = {
+
+      gitsigns = { enabled = true },
+      lualine = { enabled = true },
+
+    },
+  },
+}
