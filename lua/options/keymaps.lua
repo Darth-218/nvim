ef295eeb55630218e5e101cd90beb7faec2b50de
Darth218 <64696569+Darth-218@users.noreply.github.com>
2024-05-03 16:18:20 +0300

    Removed useless binds and fixed telescope find bind

--- lua/options/keymaps.lua
+++ lua/options/keymaps.lua
@@ -7,8 +7,6 @@ vim.keymap.set("n", "<C-h>", "<C-w>h", {noremap = true})
 -- In-file scrolling
 vim.keymap.set("n", "<C-d>", "<C-d>zz", {noremap = true})
 vim.keymap.set("n", "<C-u>", "<C-u>zz", {noremap = true})
-vim.keymap.set("n", "<C-e>", "<C-e>j", {noremap = true})
-vim.keymap.set("n", "<C-y>", "<C-y>k", {noremap = true})
 
 -- Copying to clipboard
 vim.keymap.set("v", "<leader>y", '"+y')
@@ -29,11 +27,8 @@ vim.keymap.set("n", "<C-w>\\", "<C-w>v", {noremap = true})
 -- Oil
 vim.keymap.set("n", "-", "<cmd>Oil --float %:h<CR>", {noremap = true})
 
--- WhichKey
--- vim.keymap.set("n", "<leader>", "<cmd>WhichKey '<Space>'<CR>", { noremap = true, silent = true})
-
 -- Telescope
-vim.keymap.set("n", "<leader>tc", "<cmd>cd %:h | Telescope find_files theme=cursor hidden=true<CR>", {noremap = true})
+vim.keymap.set("n", "<leader>tf", "<cmd>cd %:h | Telescope find_files theme=ivy hidden=true<CR>", {noremap = true})
 vim.keymap.set("n", "<leader>th", "<cmd>cd | Telescope find_files theme=ivy hidden=true<CR>", {noremap = true})
 vim.keymap.set("n", "<leader>tr", "<cmd>Telescope oldfiles theme=ivy<CR>", {noremap = true})
 vim.keymap.set("n", "<leader>tg", "<cmd>Telescope git_files theme=ivy<CR>", {noremap = true})
@@ -46,13 +41,8 @@ vim.keymap.set("n", "<leader>d", "<cmd>Dashboard<CR>", {noremap = true})
 -- Undotree
 vim.keymap.set("n", "<leader>ut", vim.cmd.UndotreeToggle, {noremap = true})
 
--- fzf
--- vim.keymap.set("n", "<leader>ff", "<cmd>Files ~/<CR>")
-
 -- bufkill
 vim.keymap.set("n", "<leader>bq", "<cmd>BD<CR>") -- Kill buffer
-vim.keymap.set("n", "H", "<cmd>BufferLineCyclePrev<CR>") -- Previous buffer
-vim.keymap.set("n", "L", "<cmd>BufferLineCycleNext<CR>") -- Next buffer
 
 -- lspconfig
 vim.keymap.set("n", "K", vim.lsp.buf.hover, {noremap = true})
@@ -67,17 +57,6 @@ vim.keymap.set("n", "<leader>m", "<cmd>TodoTelescope cwd=%:h<CR>", {noremap = tr
 
 -- hop
 vim.keymap.set("n", "<leader>j", "<cmd>HopWord<CR>", {noremap = true})
-
--- bufferline
-vim.keymap.set("n", "<leader>1", "<cmd>BufferLineGoToBuffer1<CR>")
-vim.keymap.set("n", "<leader>2", "<cmd>BufferLineGoToBuffer2<CR>")
-vim.keymap.set("n", "<leader>3", "<cmd>BufferLineGoToBuffer3<CR>")
-vim.keymap.set("n", "<leader>4", "<cmd>BufferLineGoToBuffer4<CR>")
-vim.keymap.set("n", "<leader>5", "<cmd>BufferLineGoToBuffer5<CR>")
-vim.keymap.set("n", "<leader>6", "<cmd>BufferLineGoToBuffer6<CR>")
-vim.keymap.set("n", "<leader>7", "<cmd>BufferLineGoToBuffer7<CR>")
-vim.keymap.set("n", "<leader>8", "<cmd>BufferLineGoToBuffer8<CR>")
-vim.keymap.set("n", "<leader>9", "<cmd>BufferLineGoToBuffer9<CR>")
 
 -- Silicon
 vim.keymap.set("n", "<C-s>", "<cmd>cd %:h | Silicon<CR>")
