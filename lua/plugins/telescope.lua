51fcd9a3b783d96e9ce7114eb244c2abe8adfaf7
Darth218 <64696569+Darth-218@users.noreply.github.com>
2024-05-03 16:16:27 +0300

    Changed file formmating

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,32 +1,32 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.6",
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.6",
 
-		dependancies = { "nvim-lua/plenary.nvim" },
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
+    dependancies = { "nvim-lua/plenary.nvim" },
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
 
-		config = function()
-			require("telescope").setup({
-				pickers = {
-					find_files = {
-						"fd",
-						".",
-						"--type",
-						"file",
-						"--hidden",
-						"--strip-cwd-prefix",
-					},
-				},
-			})
-			extensions = {
-				["ui-select"] = {
-					require("telescope.themes").get_ivy({}),
-				},
-			}
-			-- require("telescope").load_extension("ui-select")
-		end,
+    config = function()
+      require("telescope").setup({
+	pickers = {
+	  find_files = {
+	    "fd",
+	    ".",
+	    "--type",
+	    "file",
+	    "--hidden",
+	    "--strip-cwd-prefix",
+	  },
 	},
+      })
+      extensions = {
+	["ui-select"] = {
+	  require("telescope.themes").get_ivy({}),
+	},
+      }
+      -- require("telescope").load_extension("ui-select")
+    end,
+  },
 }
