1b1624561ddc4aacb6f9cf8c271c1a5680148e12
darth-218 <yahia.hany.gaber@proton.me>
2024-07-03 00:58:25 +0300

    Added obsidian.nvim

---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,21 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*",
+  lazy = true,
+  ft = "markdown",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "personal",
+        path = "~/the-duat/Notes",
+      },
+      {
+	name = "uni",
+	path = "~/the-duat/uni"
+      }
+    },
+  },
+}
